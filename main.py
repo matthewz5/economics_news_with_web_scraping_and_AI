8508c23d2e212c58245c589253dda574c05d081e
Matheus Henrique da Silva <98927768+matthewz5@users.noreply.github.com>
2025-02-09 16:07:25 -0300

    add e-mail sending code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.config import URL, API_KEY, NEWS_EXCEL_PATH, SUMMARY_MD_PATH
 from src.data_collection import get_content, scrap_data
 from src.data_processing import process_news_data
 from src.summarizer_ai import summarize_news
+from src.send_email import send_email
 
 print("Scraping news...")
 soup = get_content(URL)
@@ -13,4 +14,5 @@ df = process_news_data(news, NEWS_EXCEL_PATH)
 print("Summarizing news...")
 summary = summarize_news(df, API_KEY, SUMMARY_MD_PATH)
 
-print(summary.text)
+print("Sending news email...")
+send_email(f"Resumo de notícias econômicas semanal SUNO", "mail here", summary.text)
